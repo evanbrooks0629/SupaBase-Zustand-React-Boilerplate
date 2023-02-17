b2d12c43905b5528da735505fd3b45814edc3db2
evanbrooks0629 <60624108+evanbrooks0629@users.noreply.github.com>
2023-02-17 17:11:30 -0500

    Update supabaseClient.js

--- src/data/supabaseClient.js
+++ src/data/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // load these from somewhere else eventually
-const supabaseUrl = "https://vsfdwikfdhteyktahnda.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZzZmR3aWtmZGh0ZXlrdGFobmRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzU3MTE1MjgsImV4cCI6MTk5MTI4NzUyOH0.DxI-YV96BThC5Y2kHqMO2_67awc_8OvUmGz0xy6yK5A";
+const supabaseUrl = "your_supabase_url";
+const supabaseAnonKey = "your_supabase_anon_key";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
